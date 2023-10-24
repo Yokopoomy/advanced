977846feb05cce600a0bf3762c9dd19087fa4d81
Evgeny Golovin <yokopoomy20@gmail.com>
2023-10-24 20:30:04 +0300

    Delete OldFunc

--- src/js/app.js
+++ src/js/app.js
@@ -3,38 +3,20 @@ function sortByHealth(items) {
 }
 
 export default function orderByProps(obj, args) {
-  let arr = []
-    for(let key in obj) {
-        let obj2 = {};
-          obj2.key = key;
-          obj2.value = obj[key];
-          arr.push(obj2);
+  const arr = [];
+  for (const key in obj) {
+    const obj2 = {};
+    obj2.key = key;
+    obj2.value = obj[key];
+    arr.push(obj2);
   }
-  arr.sort((a, b) => a.key > b.key ? 1 : -1)
-  for(let i = args.length - 1; i >= 0; i--) {
-    for(let k = 0; k < arr.length; k++){
-      if(arr[k].key == args[i]){
-        arr.unshift(...arr.splice(k,1));
+  arr.sort((a, b) => (a.key > b.key ? 1 : -1));
+  for (let i = args.length - 1; i >= 0; i--) {
+    for (let k = 0; k < arr.length; k++) {
+      if (arr[k].key == args[i]) {
+        arr.unshift(...arr.splice(k, 1));
       }
     }
   }
-  return arr
-}
-
-function orderByProps2(obj, args) {
-  let arr = []
-    for(let i = 0; i < args.length; i++){
-      for(let key in obj) {
-        if(key == args[i]){
-          let obj2 = {};
-          obj2.key = key;
-          obj2.value = obj[key];
-          arr.push(obj2)
-          
-      }
-      
-    }
-  }
-  console.log(arr)
-  return arr
+  return arr;
 }
