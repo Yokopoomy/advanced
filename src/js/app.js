eec5bc49e2d1848d169888c5e33efe232109c8e6
Evgeny Golovin <yokopoomy20@gmail.com>
2023-10-24 20:40:22 +0300

    Fix eslint

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,17 @@
-function sortByHealth(items) {
-  return items.sort((x, y) => y.health - x.health);
-}
-
 export default function orderByProps(obj, args) {
   const arr = [];
   for (const key in obj) {
-    const obj2 = {};
-    obj2.key = key;
-    obj2.value = obj[key];
-    arr.push(obj2);
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const obj2 = {};
+      obj2.key = key;
+      obj2.value = obj[key];
+      arr.push(obj2);
+    }
   }
   arr.sort((a, b) => (a.key > b.key ? 1 : -1));
-  for (let i = args.length - 1; i >= 0; i--) {
-    for (let k = 0; k < arr.length; k++) {
-      if (arr[k].key == args[i]) {
+  for (let i = args.length - 1; i >= 0; i -= 1) {
+    for (let k = 0; k < arr.length; k += 1) {
+      if (arr[k].key === args[i]) {
         arr.unshift(...arr.splice(k, 1));
       }
     }
