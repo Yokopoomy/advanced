c09e55a1129a8c58c498314f04a6cdcd4d165b8f
Evgeny Golovin <yokopoomy20@gmail.com>
2023-10-24 20:53:39 +0300

    Error lint

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,10 @@
 export default function orderByProps(obj, args) {
   const arr = [];
   for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      const obj2 = {};
-      obj2.key = key;
-      obj2.value = obj[key];
-      arr.push(obj2);
-    }
+    const obj2 = {};
+    obj2.key = key;
+    obj2.value = obj[key];
+    arr.push(obj2);
   }
   arr.sort((a, b) => (a.key > b.key ? 1 : -1));
   for (let i = args.length - 1; i >= 0; i -= 1) {
